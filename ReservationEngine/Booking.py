0b545d4b57413b8975c7e36c2b2d825f01a58aa8
Kiiara <kiaracobanova@gmail.com>
2023-05-16 20:31:02 +0300

    Added error handling in Booking.py

--- ReservationEngine/Booking.py
+++ ReservationEngine/Booking.py
@@ -8,51 +8,73 @@ class Booking:
     def __init__(self):
         self.attribute = 0
                 
+
     def driverConnect(self):
         options = Options()
         options.add_experimental_option("detach", True)
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-        return driver
-    
-    def openHalls(self, pageName, driver):  
+
+        try:
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+            driver.implicitly_wait(10)  
+            return driver
+        except Exception as e:
+            print("Failed to connect to the driver:", str(e))
+            return None
+        
+        
+    def openHalls(self, pageName, driver):
         driver.get(pageName)
         driver.maximize_window()
 
-        linksForCookies = driver.find_elements("xpath", "//div[contains(@class, 'cookie-policy-action')]")
-
-        for link in linksForCookies:
-            if "Приемам" in link.get_attribute("innerHTML"):
-                link.click()
-                break
-            
-        mainLinks = driver.find_elements("xpath", "//a[@href]")
-        for link in mainLinks:
-            if "Зали" in link.get_attribute("innerHTML"):
-                link.click()
-                break
-            
-    def openWantedHall(self, driver, wantedHall):    
-        hallsLinks = driver.find_elements("xpath", "//a[@href]")
-        for link in hallsLinks:
-            if wantedHall in link.get_attribute("innerHTML"):
-                link.click()
-                break         
-            
-    def goToEvent(self, driver, eventName):
-        allEventInHallLinks = driver.find_elements("xpath", "//div[contains(@class, 'a-tabPanel')]")
-        for event_link in allEventInHallLinks:
-            links = event_link.find_elements("xpath", "//a[@href]")
-            for link in links:
-                if eventName in link.get_attribute("href"):
+        try:
+            linksForCookies = driver.find_elements("xpath", "//div[contains(@class, 'cookie-policy-action')]")
+            for link in linksForCookies:
+                if "Приемам" in link.get_attribute("innerHTML"):
                     link.click()
                     break
-                # else  
-                
-                
+        except Exception as e:
+            print("Error handling cookies:", str(e))
+
+        try:
+            mainLinks = driver.find_elements("xpath", "//a[@href]")
+            for link in mainLinks:
+                if "Зали" in link.get_attribute("innerHTML"):
+                    link.click()
+                    break
+        except Exception as e:
+            print("Error opening halls:", str(e))
+            
+            
+    def openWantedHall(self, driver, wantedHall):
+        try:
+            hallsLinks = driver.find_elements("xpath", "//a[@href]")
+            for link in hallsLinks:
+                if wantedHall in link.get_attribute("innerHTML"):
+                    link.click()
+                    break
+        except Exception as e:
+            print("Error opening the wanted hall:", str(e))    
+            
+            
+    def goToEvent(self, driver, eventName):
+        try:
+            allEventInHallLinks = driver.find_elements("xpath", "//div[contains(@class, 'a-tabPanel')]")
+            for event_link in allEventInHallLinks:
+                links = event_link.find_elements("xpath", "//a[@href]")
+                for link in links:
+                    if eventName in link.get_attribute("href"):
+                        link.click()
+                        return  
+            print("Event not found:", eventName)
+        except Exception as e:
+            print("Error navigating to the event:", str(e))
+
+
 booking = Booking()
 
 driver = booking.driverConnect()
+print("Driver object returned successfully!")
 pageName = "https://www.eventim.bg/bg/"
 booking.openHalls(pageName, driver)
 wantedHall = "София"
@@ -62,5 +84,3 @@ eventName = "/bg/bileti/cska-balkan-sofiya-basketbolna-zala-594635/event.html"
 booking.goToEvent(driver, eventName)
 
 # date and time format: 2023-05-15T19:00:00+03:00
-
-    
